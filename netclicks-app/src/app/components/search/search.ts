71d00be7dca4e3f9573389a63edd45c150885766
AnnaDobryakova <dobryakova500@mail.ru>
2025-09-23 21:05:44 +0300

    Angular. Поиск, Роутинг

--- netclicks-app/src/app/components/search/search.ts
+++ netclicks-app/src/app/components/search/search.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Films } from '../../services/films';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-search',
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './search.css'
 })
 export class Search {
+  public searchString: string = ''
 
+  constructor( private _filmsService: Films) {
+  }
+
+  public updateValue(event: Event) {
+    const target = event.target as HTMLInputElement
+    this.searchString = target.value
+  }
+
+  public updateSearch(event: Event) {
+    this._filmsService.searchString = this.searchString
+  }
 }
